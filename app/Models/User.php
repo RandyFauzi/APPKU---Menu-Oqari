9b6b94365d3302be3380f4ed915499be674b8a76
Randy Fauzi <randyfauzi24@gmail.com>
2026-09-02 11:20:43 +0800

    fix: add shop relationship to User model to resolve SuperAdmin dashboard crash

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function shop()
+    {
+        return $this->belongsTo(Shop::class);
+    }
 }
+
