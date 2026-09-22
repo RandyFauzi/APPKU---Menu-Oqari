392412ecebcac7a225093da01fc0ef403fefe2b7
RandyFauzi <RandyFauzi@users.noreply.github.com>
2026-09-22 08:38:38 +0800

    fix: revert public disk root to public_path('uploads') for shared hosting compatibility

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,8 +40,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => '/public/storage',
+            'root' => public_path('uploads'),
+            'url' => '/uploads',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
